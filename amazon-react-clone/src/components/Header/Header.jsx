dbba05ba2516a71cf960242a88e8f5a234fc94c5
Aman Mulani <amanmulani369@gmail.com>
2022-11-23 15:51:24 +0530

    WIP | AM | Rough Header Component Draft

--- amazon-react-clone/src/components/Header/Header.jsx
+++ amazon-react-clone/src/components/Header/Header.jsx
@@ -11,19 +11,25 @@ export const Header = () => {
                 Deliver to Safa Mulani
             </div>
             <div className="header-div search-div">
+                <div className="search-dropdown">
+
+                </div>
                 <input className="search-box" type="text" />
+                <div className="search-button">
+
+                </div>
             </div>
             <div className="header-div">
-
+                EN
             </div>
             <div className="header-div">
-
+                Accounts & Lists
             </div>
             <div className="header-div">
-
+                Returns & Orders
             </div>
             <div className="header-div">
-
+                Cart
             </div>
         </div>
     );
